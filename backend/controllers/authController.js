dc6c1a3e8be05fd08961820fea1fbdfbd83eff4b
div-zihad007 <div.sajid007@gmail.com>
2024-08-23 23:41:40 +0600

    sign api complete

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 import userModel from "../models/userModel.js";
 import bcrypt from "bcrypt";
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -32,4 +33,24 @@ const signUp = async (req, res, next) => {
   }
 };
 
-export { signUp };
+const signIn = async (req,res,next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await userModel.findOne({email});
+    if(!validUser) return next(errorHandler(404,"User not found!"));
+    
+    const validPassword = await bcrypt.compare(password,validUser.password);    
+    if(!validPassword) return next(errorHandler(401,"Password not matched!"));
+
+    const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+    const {password:pass,...rest} = validUser._doc;
+    res.cookie("access_token",token,{httpOnly:true}).status(200).json(rest);
+
+
+  } catch (error) {
+    next(error);
+  }
+}
+
+export { signUp, signIn };
